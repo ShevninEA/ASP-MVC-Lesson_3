26e404cdaec6593588b5897b2ee7a4f884d25329
ShevninEA <ev.shevnin@yandex.ru>
2022-12-24 17:46:58 +0300

    Что то типо домашнего задания

---
+++ BaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_MVC_Lesson_3
+{
+    public abstract class BaseClass
+    {
+        public string Name { get; set; }
+        public string TypeWorker { get; set; }
+
+        public abstract decimal MonthlySalary(decimal Rate);
+
+        public string[] employee = new string[6];
+    }
+}
---
+++ FixWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_MVC_Lesson_3
+{
+    public class FixWorker : BaseClass
+    {
+        public override decimal MonthlySalary(decimal Rate)
+        {
+            decimal monthlySalary = Rate;
+            return monthlySalary;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(employee[3] = $"{Name = "Женя"} / {TypeWorker = "Полная ставка"} / Зарплата = {MonthlySalary(35000)} руб.");
+            Console.WriteLine(employee[4] = $"{Name = "Миша"} / {TypeWorker = "Полная ставка"} / Зарплата = {MonthlySalary(45000)} руб.");
+            Console.WriteLine(employee[5] = $"{Name = "Оля"} / {TypeWorker = "Полная ставка"} / Зарплата = {MonthlySalary(50000)} руб.");
+        }
+    }
+}
